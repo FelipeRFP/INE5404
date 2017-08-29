ae9eaa7ad79a44a24e3f6c41949f58b3a49cba8d
Felipe Reis Felix Pagliarini <lipreis98@gmail.com>
2017-08-29 09:52:35 -0300

    Added Maven Project

---
+++ exercises/src/main/java/INE5404/FelipeReis/App.java
@@ -0,0 +1,13 @@
+package INE5404.FelipeReis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
